f8e09f1d258ebb6454f0f4e6a101b96220622913
benjfdup <benfdup@gmail.com>
2023-07-23 15:45:04 -0400

    Began work on address list autocomplete widget

---
+++ lib/components/address_list.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+/* This is where the autocomplete list widget will go... */
+
+class AddressList extends StatelessWidget {
+  const AddressList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column();
+  }
+}
