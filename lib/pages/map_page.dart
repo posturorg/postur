10602660bf114ab86f24bc85373f4cc45a1ba944
benjfdup <benfdup@gmail.com>
2023-08-11 20:57:48 -0400

    reformat

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -20,12 +20,13 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-
   // Retrieve current user's uid
   final String uid = FirebaseAuth.instance.currentUser!.uid;
 
   // Retrieve current user document
-  DocumentReference currentUser = FirebaseFirestore.instance.collection('Users').doc(FirebaseAuth.instance.currentUser!.uid);
+  DocumentReference currentUser = FirebaseFirestore.instance
+      .collection('Users')
+      .doc(FirebaseAuth.instance.currentUser!.uid);
 
   // Retrieve events to be shown on the map
   final Query events = FirebaseFirestore.instance.collection('Events');
@@ -38,7 +39,7 @@ class _MapPageState extends State<MapPage> {
     mapController = controller;
     mapController.setMapStyle(mapStyleString);
   }
-  
+
   Future<void> _showCreateModal(LatLng location) async {
     //CREATE EVENT MODAL
     //UI PROBLEM, NEED TO FIX WHAT HAPPENS WHEN A KEYBOARD IS PULLED UP
@@ -77,103 +78,116 @@ class _MapPageState extends State<MapPage> {
       _showCreateModal(location);
     });
   }
-  
+
   @override
   Widget build(BuildContext context) {
     // Stream current user data to get list of events you're invitd to
     return StreamBuilder<DocumentSnapshot>(
-      stream: currentUser.snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          // Show Waiting Indicator
-          return const Center(child: CircularProgressIndicator(color: absentRed,));
-  
-        // What to show if data has been received
-        } else if (snapshot.connectionState == ConnectionState.active || snapshot.connectionState == ConnectionState.done) {
-          // Potenital error message
-          if (snapshot.hasError) {
-            return const Center(child: Text("Error Occured"));
-          // Success
-          } else if (snapshot.hasData) {
-            // Save sets of events user is either invited to or attending (every element in attending should be inside of invited)
-            Set<String> invitedEventIds = Set.from(snapshot.data!['invited'] ?? []);
-            Set<String> attendingEventIds = Set.from(snapshot.data!['attending'] ?? []);
+        stream: currentUser.snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            // Show Waiting Indicator
+            return const Center(
+                child: CircularProgressIndicator(
+              color: absentRed,
+            ));
 
-            return StreamBuilder<QuerySnapshot>(
-              stream: events.snapshots(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  // Show Waiting Indicator
-                  return const Center(child: CircularProgressIndicator(color: absentRed,));
-          
-                // What to show if data has been received
-                } else if (snapshot.connectionState == ConnectionState.active || snapshot.connectionState == ConnectionState.done) {
-                  // Potenital error message
-                  if (snapshot.hasError) {
-                    return const Center(child: Text("Error Occured"));
-                  // Success
-                  } else if (snapshot.hasData) {
-                    // 
-                    Set<Marker> markers = snapshot.data!.docs
-                    .where((event) => invitedEventIds.contains(event.id) || attendingEventIds.contains(event.id))
-                    .map((event) {
-                      // Retrieve GeoPoint location from backend
-                      GeoPoint location = event['where'] as GeoPoint;
-                      // Convert to LatLng
-                      LatLng position = LatLng(location.latitude, location.longitude);
-                      // Return each marker
-                      return Marker(
-                        // Set marker ID equal to eventId
-                        markerId: MarkerId(event.id),
-                        position: position,
-                        onTap: () {
-                          //THIS FUNCTION SHOWS THE MODAL
-                          showModalBottomSheet<void>(
-                            // context and builder are
-                            // required properties in this widget
-                            context: context,
-                            isScrollControlled: true,
-                            elevation: 0.0,
-                            backgroundColor: Colors.white,
-                            clipBehavior: Clip.antiAlias,
-                            showDragHandle: true,
-                            builder: (BuildContext context) {
-                              //Marker details MODAL START (IT IS THE SIZED BOX)
-                              return EventDetailsModal(
-                                //Change this if you made it...
-                                eventTitle: event['eventTitle'],
-                                creator: event['creator'],
-                                isCreator: event['creator'] == uid ? true : false,
-                                isMember: attendingEventIds.contains(event.id),
+            // What to show if data has been received
+          } else if (snapshot.connectionState == ConnectionState.active ||
+              snapshot.connectionState == ConnectionState.done) {
+            // Potenital error message
+            if (snapshot.hasError) {
+              return const Center(child: Text("Error Occured"));
+              // Success
+            } else if (snapshot.hasData) {
+              // Save sets of events user is either invited to or attending (every element in attending should be inside of invited)
+              Set<String> invitedEventIds =
+                  Set.from(snapshot.data!['invited'] ?? []);
+              Set<String> attendingEventIds =
+                  Set.from(snapshot.data!['attending'] ?? []);
+
+              return StreamBuilder<QuerySnapshot>(
+                  stream: events.snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      // Show Waiting Indicator
+                      return const Center(
+                          child: CircularProgressIndicator(
+                        color: absentRed,
+                      ));
+
+                      // What to show if data has been received
+                    } else if (snapshot.connectionState ==
+                            ConnectionState.active ||
+                        snapshot.connectionState == ConnectionState.done) {
+                      // Potenital error message
+                      if (snapshot.hasError) {
+                        return const Center(child: Text("Error Occured"));
+                        // Success
+                      } else if (snapshot.hasData) {
+                        //
+                        Set<Marker> markers = snapshot.data!.docs
+                            .where((event) =>
+                                invitedEventIds.contains(event.id) ||
+                                attendingEventIds.contains(event.id))
+                            .map((event) {
+                          // Retrieve GeoPoint location from backend
+                          GeoPoint location = event['where'] as GeoPoint;
+                          // Convert to LatLng
+                          LatLng position =
+                              LatLng(location.latitude, location.longitude);
+                          // Return each marker
+                          return Marker(
+                            // Set marker ID equal to eventId
+                            markerId: MarkerId(event.id),
+                            position: position,
+                            onTap: () {
+                              //THIS FUNCTION SHOWS THE MODAL
+                              showModalBottomSheet<void>(
+                                // context and builder are
+                                // required properties in this widget
+                                context: context,
+                                isScrollControlled: true,
+                                elevation: 0.0,
+                                backgroundColor: Colors.white,
+                                clipBehavior: Clip.antiAlias,
+                                showDragHandle: true,
+                                builder: (BuildContext context) {
+                                  //Marker details MODAL START (IT IS THE SIZED BOX)
+                                  return EventDetailsModal(
+                                    //Change this if you made it...
+                                    eventTitle: event['eventTitle'],
+                                    creator: event['creator'],
+                                    isCreator:
+                                        event['creator'] == uid ? true : false,
+                                    isMember:
+                                        attendingEventIds.contains(event.id),
+                                  );
+                                },
                               );
                             },
                           );
-                        },
-                      );
-                    }).toSet();
-          
-                    return GoogleMap(
-                      myLocationButtonEnabled: true,
-                      myLocationEnabled: true,
-                      compassEnabled: false,
-                      onLongPress: _onMapHold,
-                      onMapCreated: _onMapCreated,
-                      initialCameraPosition: CameraPosition(
-                        target: _center,
-                        zoom: 15.0,
-                      ),
-                      markers: markers,
-                    );
-                  }
-                }
-                return const Center(child: Text("No Data Received"));
-              }
-            );
+                        }).toSet();
+
+                        return GoogleMap(
+                          myLocationButtonEnabled: true,
+                          myLocationEnabled: true,
+                          compassEnabled: false,
+                          onLongPress: _onMapHold,
+                          onMapCreated: _onMapCreated,
+                          initialCameraPosition: CameraPosition(
+                            target: _center,
+                            zoom: 15.0,
+                          ),
+                          markers: markers,
+                        );
+                      }
+                    }
+                    return const Center(child: Text("No Data Received"));
+                  });
+            }
           }
-        }
-        return const Center(child: Text("No Data Received"));
-      }
-    );
+          return const Center(child: Text("No Data Received"));
+        });
   }
 }
-
