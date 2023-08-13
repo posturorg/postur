03bda3db9a8047a5f323325912280d7a2f7887b1
alvinadjei <alvinadjei@college.harvard.edu>
2023-08-13 14:25:24 -0400

    Events Backend Switch completed

--- lib/firestore.dart
+++ lib/firestore.dart
@@ -12,21 +12,12 @@ Future<void> userSetup(String email) async {
   // Create map of initial user values
   final Map<String, dynamic> userMap = {
     'name': {'first':'', 'last':''},
-    // Remove 'first_name' and 'last_name'
-    'first_name': '',
-    'last_name': '',
     // Maybe use an encryption key to change default username to something more secure later
     'username': uid,
     'email': email,
     'uid': uid,
-    // Rename "profile_pic" to profilePic
     'profile_pic': "",
-    // Remove 'invited'
-    'invited': [],
-    // Remove 'attending'
-    'attending': [],
     'contacts': {},
-    'tags': [],
     'accountType': 'private',
     'hasName': false,
     'hasUsername': false,
