771695d10c616885c7cecefdd1b9616a5342e71d
benjfdup <benfdup@gmail.com>
2023-08-13 12:45:05 -0400

    added contact permissions page

---
+++ lib/pages/contacts_permissions_page.dart
@@ -0,0 +1,62 @@
+import 'package:auth_test/pages/home_page.dart';
+import 'package:auth_test/src/colors.dart';
+import 'package:flutter/material.dart';
+
+class ContactsPermissionsPage extends StatefulWidget {
+  const ContactsPermissionsPage({super.key});
+
+  @override
+  State<ContactsPermissionsPage> createState() =>
+      _ContactsPermissionsPageState();
+}
+
+class _ContactsPermissionsPageState extends State<ContactsPermissionsPage> {
+  bool hasAllowedContacts = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // add fetch for hasAllowedContacts on the backend here...
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return hasAllowedContacts
+        ? const HomePage()
+        : Scaffold(
+            appBar: AppBar(
+              title: const Text(
+                'Contact Permissions',
+                style: TextStyle(
+                  color: absentRed,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Please grant access to your contacts.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'This is only used to to friend people you already know.',
+                  textAlign: TextAlign.center,
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    print('done pressed!');
+                  },
+                  icon: const Icon(Icons.check_box_rounded),
+                  label: const Text('Done'),
+                ),
+              ],
+            ),
+          );
+  }
+}
